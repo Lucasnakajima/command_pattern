a941e286ebb351b1683bd08615d652d20a3d6b02
up202001337 <nakajima.lucas02@gmail.com>
2022-11-18 15:50:33 +0000

    done StringRecipe

--- src/main/java/StringRecipe.java
+++ src/main/java/StringRecipe.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class StringRecipe implements StringTransformer{
+public class StringRecipe{
     private List<StringTransformer> transformers;
     public StringRecipe(List<StringTransformer> transformers) {
         this.transformers = transformers;
@@ -10,11 +10,18 @@ public class StringRecipe implements StringTransformer{
         return transformers;
     }
 
-    @Override
-    public void execute(StringDrink drink) {
-
-    }
-
     public void mix(StringDrink drink) {
+        for (int i = 0; i < transformers.size(); i++) {
+            if(transformers.get(i).getClass() == StringInverter.class){
+                StringInverter si = new StringInverter();
+                si.execute(drink);
+            } else if (transformers.get(i).getClass() == StringCaseChanger.class) {
+                StringCaseChanger cc = new StringCaseChanger();
+                cc.execute(drink);
+            } else {
+                StringReplacer sr = new StringReplacer('A', 'X');
+                sr.execute(drink);
+            }
+        }
     }
 }
